3596c14d34b30ffc4d07ead475dc7e0ae3269966
PASHAM RAKSHITH REDDY <148862970+Pashamrakshithreddy@users.noreply.github.com>
2023-11-07 22:38:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function switchToSignup() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('signupForm').style.display = 'block';
+  }
+  
+  function switchToLogin() {
+    document.getElementById('signupForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'block';
+  }
